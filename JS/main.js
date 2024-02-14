8d3fda8883321de81bd0960485c4a18899fc0b91
AhmedMorsy <20192234@fue.edu.eg>
2024-02-14 22:00:39 +0200

    main

--- JS/main.js
+++ JS/main.js
@@ -25,7 +25,11 @@ const searchInput = document.getElementById("searchInput");
 const findBtn = document.getElementById("findBtn");
 const today = document.getElementById("today");
 const currentWeather = document.getElementById("currentWeather");
-
+const tomorrow = document.getElementById("tomorrow");
+const tomorrowWeather = document.getElementById("tomorrowWeather");
+const the_day_after = document.getElementById("the_day_after");
+const thedayafterWeather = document.getElementById("thedayafterWeather");
+const map = document.getElementById("map");
 (function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPosition);
@@ -44,20 +48,39 @@ $(function () {
     });
   });
 });
+
 // API Link
 // Set default value
 getWeatherApi("Cairo");
+
+function displayMap(city) {
+  map.innerHTML = `
+  <div class="container">
+  <div class="rounded-3 overflow-hidden mb-3" style="width: 100%">
+      <iframe
+        width="100%"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=${city}&amp;t=&amp;z=7&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      </iframe>
+    </div>
+  </div>`;
+}
+
 async function getWeatherApi(city) {
   try {
     let http = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=79bfff5c2aea4f6295f142203240901&q=${city}&days=7`
+      `https://api.weatherapi.com/v1/forecast.json?key=79bfff5c2aea4f6295f142203240901&q=${city}&days=3`
     );
 
     if (http.ok && http.status !== 400) {
       let response = await http.json();
-      console.log(response);
       displayCity(response.location, response.current);
       displayNext(response.forecast.forecastday);
+      displayMap(city);
     } else {
       console.log("Something wrong happened");
     }
@@ -83,31 +106,63 @@ function displayCity(data, temp) {
   }</p> </div>`;
   currentWeather.innerHTML = `<div class="city"><p> ${data.name} ,${data.region}</p></div>  
   <div class="forecast-degree d-flex align-content-center justify-content-center">
-  <div class="num" id="num"><p>${temp.temp_c}<sup>o</sup>C</p></div>
-  <div class="icon" id="icon"><img  src="${temp.condition.icon}"  class='w-100'/></div>
-  </div> <div class="forecast-status text-center"> <p> ${temp.condition.text} </p></div>
-  <div class="forecast-foot">
-  <div class="row text-center">
-  <div class="col-sm-4 col-md-4"><i class="fa-solid fa-umbrella"></i> ${temp.cloud} % </div>
-  <div class="col-sm-4 col-md-4"> <i class="fa-solid fa-wind"></i> ${temp.wind_kph} km/h</div>
-  <div class="col-sm-4 col-md-4"><i class="fa-regular fa-compass"></i>${temp.wind_dir}</div>
-  </div></div>`;
+    <div class="num" id="num"><p>${temp.temp_c}<sup>o</sup>C</p></div>
+    <div class="icon" id="icon"><img  src="${temp.condition.icon}"  class='w-100'/></div>
+  </div> 
+  <div class="forecast-status text-center"> <p> ${temp.condition.text} </p></div>
+  <div class="forecast-foot text-center">
+    <span> <i class="fa-solid fa-umbrella"></i> ${temp.cloud} % </span>
+    <span> <i class="fa-solid fa-wind"></i> ${temp.wind_kph} km/h </span>
+    <span> <i class="fa-regular fa-compass"></i>${temp.wind_dir}</span>
+  </div>`;
 }
 
 function displayNext(forecast) {
-  let x = ``;
-  for (let i = 1; i < forecast.length; i++) {
-    x += `
-    <tr>
-      <td> ${days[new Date(forecast[i].date.replace(" ", "T")).getDay()]}</td>
-      <td >  ${forecast[i].day.condition.text}  <img src='${
-      forecast[i].day.condition.icon
-    }'/></td>
-      <td> ${forecast[i].day.mintemp_c} <sup>o</sup> C</td> 
-      <td>  ${forecast[i].day.maxtemp_c} <sup>o</sup> C</td>
-    </tr>
-    `;
-  }
+  // Tomorrow Begin
+  const tomorrowDate = new Date();
+  tomorrow.innerHTML = `
+  <div class="day"> <p>${days[tomorrowDate.getDay() + 1]}</p></div>
+  <div class="date"><p>${tomorrowDate.getDate() + 1} ${
+    monthNames[tomorrowDate.getMonth()]
+  }</p> </div> `;
 
-  document.getElementById("tableData").innerHTML = x;
+  tomorrowWeather.innerHTML = `
+  <div class='forecast-degree mt-5 d-flex align-content-center justify-content-center'>
+    <div class='num'> <p> ${forecast[1].day.maxtemp_c}<sup>o</sup>C </p>  </div>
+    <div class='icon'> <img src='${forecast[1].day.condition.icon}'  class='w-100'  /> </div>
+  </div>
+  <div>
+    <p> ${forecast[1].day.mintemp_c}<sup>o</sup>C </p>
+  </div>
+  <div class="status text-center"> <p> ${forecast[1].day.condition.text}</p> </div>
+  <div class="forecast-foot text-center">
+    <span> <i class="fa-solid fa-umbrella"></i> ${forecast[1].day.daily_chance_of_rain} % </span>
+    <span> <i class="fa-solid fa-wind"></i> ${forecast[1].day.maxwind_kph} km/h </span>
+  </div>
+  `;
+  // Tomorrow End
+
+  // The_day_after Begin
+  const tda = new Date();
+  the_day_after.innerHTML = `
+  <div class="day"> <p>${days[tda.getDay() + 2]}</p></div>
+  <div class="date"><p>${tda.getDate() + 2} ${
+    monthNames[tda.getMonth()]
+  }</p> </div> `;
+
+  thedayafterWeather.innerHTML = `
+  <div class='forecast-degree mt-5 d-flex align-content-center justify-content-center'>
+    <div class='num'> <p> ${forecast[2].day.maxtemp_c}<sup>o</sup>C </p>  </div>
+    <div class='icon'> <img src='${forecast[2].day.condition.icon}'  class='w-100'  /> </div>
+  </div>
+  <div>
+    <p> ${forecast[2].day.mintemp_c}<sup>o</sup>C </p>
+  </div>
+  <div class="status text-center"> <p> ${forecast[2].day.condition.text}</p> </div>
+  <div class="forecast-foot text-center">
+    <span> <i class="fa-solid fa-umbrella"></i> ${forecast[2].day.daily_chance_of_rain} % </span>
+    <span> <i class="fa-solid fa-wind"></i> ${forecast[2].day.maxwind_kph} km/h </span>
+  </div>
+  
+  `;
 }
